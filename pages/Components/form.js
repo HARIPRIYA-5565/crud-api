0a4684307c955be6a780bd813540692438395e2a
HARIPRIYA-5565 <ikavyanshi@gmail.com>
2022-06-22 10:51:34 +0530

    uploaded again

--- pages/Components/form.js
+++ pages/Components/form.js
@@ -2,28 +2,43 @@ import React from 'react';
 import {useState} from 'react';
 import {useRouter} from 'next/router'
 
-export  default  function form(props){
+export  default  function form(){
+const [page,setPage]=useState([])
+const [datas,setDatas]=useState('')
 
+const fetchPage=async()=>{
+	const response=await fetch('')
+	const data=await response.json()
+	setPage(data)
+}
+const submitDatas=async()=>{
+	const response= await fetch('',{
+		method : 'POST',
+	body:JSON.stringify({}),
+	headers:{
+		'Content-Type':'application/json',
+	},
+	})
+	const data=await response.json()
+	console.log(data)
+	
+}
 return(
-    <form>
-
-		<div>
-		<label>
-			name
-		</label>
-		</div>
-    {/* <div className="max-w-md w-full mx-auto shadow rounded-lg p-7 space-y-6">
-    <div className="flex flex-col">
-						<label className="text-sm font-bold text-gray-600 mb-1">
-							
-							Name
-						</label>
-						<input
+	<form >
+        
+	<div className="max-w-md w-full mx-auto shadow rounded-lg p-7 space-y-6">
+				<div className="flex flex-col">
+					<label className="text-sm font-bold text-gray-600 mb-1">
+						
+						Name
+					</label>
+					<input
 							className=" border-2 rounded-lg px-3 py-1 hover:border-indigo-600"
 							type="text"
 							name="name"
 							id="name"
-                            
+                            value={datas}
+                            onChange={(e)=> setDatas(e.target.value)}
 							placeholder="Enter Your Name"
 						/>
 					</div>
@@ -37,14 +52,22 @@ return(
 							type="text"
 							name="username"
 							id="username"
-                           
+                            value={datas}
+                            onChange={(e)=> setDatas(e.target.value)}
 							placeholder="Enter Your Username"
 						/>
-					</div>
-                    <button className="w-28 bg-indigo-600 text-white rounded-md p-2  hover:bg-blue-700" type="submit" >Add Users
+						</div>
+                    <button className="w-28 bg-indigo-600 text-white rounded-md p-2  hover:bg-blue-700" type="submit" onClick={submitDatas}>Add Users
                      </button>
-                    </div> */}
-    </form>
+					 {page.map((datas)=>{
+						return(
+							<div ></div>
+						)
+					 }
+					 )}
+		</div>
+	</form>
+    
 )
 
 }
--- pages/index.js
+++ pages/index.js
@@ -2,40 +2,6 @@ import { form } from "./Components/form"
 
 export default function Home() {
   return (
-    <form>
-    <div className="max-w-md w-full mx-auto shadow rounded-lg p-7 space-y-6">
-    <div className="flex flex-col">
-						<label className="text-sm font-bold text-gray-600 mb-1">
-							
-							Name
-						</label>
-						<input
-							className=" border-2 rounded-lg px-3 py-1 hover:border-indigo-600"
-							type="text"
-							name="name"
-							id="name"
-                            
-							placeholder="Enter Your Name"
-						/>
-					</div>
-					<div className="flex flex-col">
-						<label className="text-sm font-bold text-gray-600 mb-1">
-						
-							Username
-						</label>
-						<input
-							className=" border-2 rounded-lg px-3 py-1 hover:border-indigo-600"
-							type="text"
-							name="username"
-							id="username"
-                           
-							placeholder="Enter Your Username"
-						/>
-					</div>
-                    <button className="w-28 bg-indigo-600 text-white rounded-md p-2  hover:bg-blue-700" type="submit" >Add Users
-                     </button>
-                    </div> 
-    </form>
     
   )
 }
